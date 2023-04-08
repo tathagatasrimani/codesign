94bcce957fa1d91198b6461fcbb172ee0ac8096c
patrickmcewen <pmcewen@stanford.edu>
2023-04-08 14:25:42 -0700

    added support for ast.augassign expressions

--- src/cfg/benchmarks/nonai_models/example.py
+++ src/cfg/benchmarks/nonai_models/example.py
@@ -3,6 +3,12 @@ def main(x, y):
     r = x + y
     q = q * r - 3
     w = q + r
-
+    if w > 0:
+        a = q + 3
+        b = a * r
+        r += a + 3 * 2
+    else:
+        a = q - 3
+        b = a / r
 if __name__ == "__main__":
     main(2, 3)
