b300be36430be464f177b68938086631cf3426f5
Vamshi Balanaga <vamshi.balanaga@eecs.berkeley.edu>
2023-10-09 11:13:14 -0700

    simulate.sh fixed, usage added to README

--- src/simulate.sh
+++ src/simulate.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
-while getopts d:n:f: flag
+while getopts qn: flag
 do
     case "${flag}" in
-        f) filepath=${OPTARG};; # $1
         n) name=${OPTARG};;
-        d) debug=${OPTARG};;
+        q)
+        QUIET=true
+        ;;
     esac
 done
 
-# arguments like this: ./simulate.sh -f benchmarks/models/<name> -n <name>
-if [ $filepath ]; then
-
-    python instrument.py $filepath
+# arguments like this: ./simulate.sh -n <name>
+if [ $name ]; then
+    FILEPATH=benchmarks/models/$name
+    python instrument.py $FILEPATH
     python instrumented_files/xformed-$name > instrumented_files/output.txt
-    if [ $debug ]; then
-        python simulate.py $filepath --notrace
+    if [ $QUIET ]; then
+        echo "Quiet mode"
+        python simulate.py $FILEPATH --notrace
     else
-        python simulate.py $filepath
+        python simulate.py $FILEPATH
     fi
-    # python simulate.py $filepath --debug $debug
-    #cd destiny/config
-    #./destiny sample.cfg
 fi
