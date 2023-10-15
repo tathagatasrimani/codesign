1461b12562a6437bcdd41e143e03be6ed014bfcb
Vamshi Balanaga <vamshi.balanaga@eecs.berkeley.edu>
2023-10-14 18:28:47 -0700

    updates

--- src/benchmarks/models/pytorch_test.py
+++ src/benchmarks/models/pytorch_test.py
@@ -32,5 +32,6 @@ def read_weights_from_file(x, y, z):
 if __name__ == "__main__":
     model = NeuralNetwork().to(device)
     W_q, W_k = read_weights_from_file(12, 28, 28)
+    z = W_k + W_q
     x = torch.rand(1, 28, 28, device=device)
     model.forward(x)
