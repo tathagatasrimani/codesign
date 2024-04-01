49fb41f575f4576f4a26234fff00428cd5f9fe59
patrickmcewen <pmcewen@stanford.edu>
2024-04-01 21:19:32 +0900

    .

--- src/benchmarks/models/bert.py
+++ src/benchmarks/models/bert.py
@@ -100,7 +100,7 @@ def self_attn(head, tokens, d_k, Q, K, V):
 
 
 def main():
-    d_model, heads, tokens, layers = 5, 12, 8, 12
+    d_model, heads, tokens, layers = 10, 12, 8, 12
     d_k = d_model // heads
     embeddings = np.random.rand(tokens, d_model)
     for i in range(tokens):
