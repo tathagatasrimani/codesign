10aef39da5e2ca99caf3b48f793ca5243d292f36
TZFC <tianzifangchen@gmail.com>
2024-05-28 04:58:33 -0400

    register decomposition for aten functions implementation in pure python

--- src/benchmarks/models/pytorch_test_inductor.py
+++ src/benchmarks/models/pytorch_test_inductor.py
@@ -8,8 +8,10 @@ from torch import nn
 import numpy as np
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+from torch._inductor.decomposition import register_decomposition
 
 device = "cpu"
+aten = torch.ops.aten
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
@@ -34,10 +36,32 @@ def read_weights_from_file(x, y, z):
     W_k = np.zeros((x, y, z))
     return W_q, W_k
 
+@register_decomposition(aten.view)
+def native_view(x, stride):
+    return [row[::stride[0]] for row in x[::stride[1]]]
+
+@register_decomposition(aten.permute)
+def native_permute(x, stride):
+    return [row[::stride[0]] for row in x[::stride[1]]]
+
+@register_decomposition(aten.relu)
+def native_relu(x):
+    return (x > 0) * x
+
+@register_decomposition(aten.addmm)
+def native_addmm(input, mat1, mat2):
+    rows_A, cols_A = len(mat1), len(mat1[0])
+    rows_B, cols_B = len(mat2), len(mat2[0])
+    result = input
+    for i in range(rows_A):
+        for j in range(cols_B):
+            for k in range(cols_A):
+                result[i][j] += mat1[i][k] * mat2[k][j]
+    return result
+
 if __name__ == "__main__":
     model = NeuralNetwork().to(device)
     W_q, W_k = read_weights_from_file(12, 28, 28)
     z = W_k + W_q
     x = torch.rand(1, 28, 28, device=device, requires_grad=True)
     model.forward(x)
-
