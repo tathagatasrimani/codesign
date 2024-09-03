19dc657a3b409ac7dc760d9eb3308b63be600ef8
Vamshi Balanaga <balanaga@stanford.edu>
2024-09-03 09:43:51 -0700

    updates

--- src/cacti_validation/high_bw_test.py
+++ src/cacti_validation/high_bw_test.py
@@ -1,67 +1,32 @@
-from multiprocessing import Pool
+# from multiprocessing import Pool
 from itertools import product
 import numpy as np
 import pandas as pd
 
 import cacti_util
 
-mem_size_range = [2048, 4096, 32768, 131072]#, 262144, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 134217728, 67108864, 1073741824]
-bw_range = [16, 32, 64] #np.concatenate((np.arange(16, 257, 16), np.arange(256, 1025, 64), np.arange(1024, 8193, 256)))
+if __name__ == "__main__":
 
-print(f"mem_size_range: {mem_size_range}")
-print(f"bw_range: {bw_range}")
+    mem_size_range = [2048, 4096, 32768] #, 131072, 262144, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 134217728, 67108864, 1073741824]
+    bw_range = [16, 32, 64] #np.concatenate((np.arange(16, 257, 16), np.arange(256, 1025, 64), np.arange(1024, 8193, 256)))
+
+    print(f"mem_size_range: {mem_size_range}")
+    print(f"bw_range: {bw_range}")
 
 
-def gen_vals_wrapper(data):
-    mem_size, bw = data
-    print(f"running mem size: {mem_size} with bw: {bw}", flush=True)
-    try:
-        return cacti_util.gen_vals(
+    data = product(mem_size_range, bw_range)
+    # print(list(data))
+    # print(list(data))
+
+    res = []
+    for mem_size, bw in list(data):
+        print(f"calling with mem: {mem_size}, bw: {bw}", flush=True)
+        res.append(cacti_util.gen_vals(
             "mem_cache",
             cacheSize=mem_size,
             blockSize=64,
             cache_type="main memory",
             bus_width=bw,
-        )
-    except Exception as e:
-        print(f"Error: {e}")
-        return pd.Series()
+        ))
 
-data = product(mem_size_range, bw_range)
-print(list(data))
-
-if __name__ == "__main__":
-    with Pool(8) as p:
-        res = p.starmap(
-            gen_vals_wrapper,
-            list(data),
-        )
-    print(res)
-    for param, series in zip(data, res):
-        print(series)
-        series['mem size'] = param[0]
-        series['bw'] = param[1]
-    res_df = pd.concat(res)
-    res_df.to_csv("cacti_validation/res.csv")
-    # with open("cacti_validation/res.txt", "w") as f:
-    #     pd.concat()
-    #     [f.write(str(series)) for series in res]
-    # print(f"len(res): {len(res)}")
-    # print(f"type(res): {type(res)}")
-    # print(f"len(res[1]): {len(res[1])}")
-    # print(f"type(res[1]): {type(res[1])}")
-    # print(f"res[1]: {res[1]}")
-    # # print(f"res[1].columns: {res[1].columns}")
-    # print(f"len(res[0]): {len(res[0])}")
-    # print(f"type(res[0]): {type(res[0])}")
-    # # print(f"res[0].columns: {res[0].columns}")
-    # print(f"res[0]: {res[0]}")
-
-
-# mem_vals = cacti_util.gen_vals(
-#     "mem_cache",
-#     cacheSize=mem_size_range[0],
-#     blockSize=64,
-#     cache_type="main memory",
-#     bus_width=bw_range[0],
-# )
+    # print(res)
