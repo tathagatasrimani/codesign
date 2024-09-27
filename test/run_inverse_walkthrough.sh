a9cca32abf78e9d7e4d410fb07225692d35119d6
patrickmcewen <pmcewen@stanford.edu>
2024-09-26 23:32:55 -0700

    scrip for inverse val walkthrough

---
+++ test/run_inverse_walkthrough.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+cd ..
+
+while getopts c:n:f:N: flag
+do
+    case "${flag}" in
+        n) name=${OPTARG};;
+        c) ARCH_CONFIG=${OPTARG};;
+        f) SAVEDIR=${OPTARG};;
+        N) NUM_ITERS=${OPTARG};;
+    esac
+done
+
+if [ $name ]; then
+    FILEPATH=src/benchmarks/models/$name.py
+    python -m src.instrument $FILEPATH
+    python -m src.instrumented_files.xformed-$name > src/instrumented_files/output.txt
+    ARGS=$FILEPATH
+    if [ $ARCH_CONFIG ]; then
+        ARGS+=" --architecture_config $ARCH_CONFIG"
+    fi
+    if [ $SAVEDIR ]; then
+        ARGS+=" --savedir $SAVEDIR"
+    fi
+    if [ $NUM_ITERS ]; then
+        ARGS+=" --num_iters $NUM_ITERS"
+    fi
+    echo $ARGS
+    python -m src.inverse_validation.inverse_validate $ARGS
+fi
