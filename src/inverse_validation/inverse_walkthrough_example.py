f21a29d74541f0494cd2d960673e891ac89eb545
patrickmcewen <pmcewen@stanford.edu>
2024-09-27 16:22:58 -0700

    walkthrough figures being generated

--- src/inverse_validation/inverse_walkthrough_example.py
+++ src/inverse_validation/inverse_walkthrough_example.py
@@ -2,6 +2,10 @@ import argparse
 import logging
 import os
 import sys
+import matplotlib.pyplot as plt
+import sympy as sp
+import numpy as np
+import itertools
 
 logger = logging.getLogger("inverse walkthrough example")
 
@@ -24,77 +28,135 @@ from src import optimize
 from src import sim_util
 from src import architecture_search
 
-args = None
-initial_tech_params = None
-final_tech_params = None
-hw = None
-scheduled_dfg = None
+class InverseWalkthrough:
+    def __init__(self):
+        self.args = None
+        self.initial_tech_params = None
+        self.hw = None
+        self.scheduled_dfg = None
+        self.dE_mult, self.dVth, self.dC_ox, self.dC_mult, self.dR_mult = [], [], [], [], []
 
-def parse_output(f):
-    # mostly copied from codesign.py
-    tech_params = {}
-    lines = f.readlines()
-    mapping = {}
-    i = 0
-    while lines[i][0] != "x":
-        i += 1
-    while lines[i][0] == "x":
-        mapping[lines[i][lines[i].find("[") + 1 : lines[i].find("]")]] = (
-            hw_symbols.symbol_table[lines[i].split(" ")[-1][:-1]]
+    def plot_3d(self, edp, symbol_list, final_tech_params):
+        tech_params_to_sub_out = {}
+        for param in final_tech_params:
+            if not (param == symbol_list[0] or param == symbol_list[1]):
+                tech_params_to_sub_out[param] = final_tech_params[param]
+
+        edp_with_two_symbols = edp.xreplace(tech_params_to_sub_out)
+
+        lam = sp.lambdify(symbol_list, edp_with_two_symbols, 'numpy')
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        x = np.linspace(0, 1, 30)
+        y = np.linspace(0, 1, 30)
+
+        points = list(itertools.product(x,y))
+        x_new = list(map(lambda p: p[0], points))
+        y_new = list(map(lambda p: p[1], points))
+        Z = list(map(lambda tup: lam(tup[0], tup[1]), points))
+
+        ax.scatter(x_new, y_new, Z)
+        plt.xlabel(symbol_list[0])
+        plt.ylabel(symbol_list[1])
+        ax.set_zlabel("EDP")
+        plt.savefig("src/inverse_validation/walkthrough_figs/3d.png")
+
+    def plot_results(self):
+        plt.plot(self.dE_mult, label="E_mult")
+        plt.plot(self.dC_ox, label="C_ox")
+        plt.plot(self.dVth, label="Vth")
+        plt.plot(self.dC_mult, label="C_mult")
+        plt.plot(self.dR_mult, label="R_mult")
+        plt.xlabel("iteration")
+        plt.ylabel("gradient value")
+        plt.legend()
+        plt.title("Gradients of different simulator variables across iterations")
+        plt.savefig("src/inverse_validation/walkthrough_figs/grads.png")
+
+    # plotting Cmult, Rmult, Emult, Vth, C_ox
+    def record_grads(self, edp, cacti_exprs, current_tech_params):
+
+        self.dC_mult.append(sp.diff(edp, hw_symbols.Ceff["Mult"]).xreplace(current_tech_params))
+        self.dR_mult.append(sp.diff(edp, hw_symbols.Reff["Mult"]).xreplace(current_tech_params))
+        print("done with R and C gradient calculation")
+        self.dVth.append(sp.diff(edp, hw_symbols.Vth).xreplace(current_tech_params))
+        print("done with Vth gradient calculation")
+        self.dC_ox.append(sp.diff(edp, hw_symbols.C_ox).xreplace(current_tech_params))
+
+        # E = CV^2 -> C = E/V^2
+        dC_dE = 1/(current_tech_params[hw_symbols.V_dd]**2)
+        self.dE_mult.append(dC_dE * self.dC_mult[-1]) # dC/dE * dEDP/dC = d/dE
+
+    def parse_output(self, f):
+        # mostly copied from codesign.py
+        tech_params = {}
+        lines = f.readlines()
+        mapping = {}
+        i = 0
+        while lines[i][0] != "x":
+            i += 1
+        while lines[i][0] == "x":
+            mapping[lines[i][lines[i].find("[") + 1 : lines[i].find("]")]] = (
+                hw_symbols.symbol_table[lines[i].split(" ")[-1][:-1]]
+            )
+            i += 1
+        while i < len(lines) and lines[i].find("x") != 4:
+            i += 1
+        i += 2
+        for _ in range(len(mapping)):
+            key = lines[i].split(":")[0].lstrip().rstrip()
+            value = float(lines[i].split(":")[2][1:-1])
+            tech_params[mapping[key]] = value  # just know that self.tech_params contains all dat
+            i += 1
+        return tech_params
+
+    def run_forward_pass(self):
+
+        # initialize hw model, override architecture config to have current tech node
+        logger.info(
+            f"Setting up architecture search; benchmark: {self.args.benchmark}, config: {self.args.architecture_config}"
         )
-        i += 1
-    while i < len(lines) and lines[i].find("x") != 4:
-        i += 1
-    i += 2
-    for _ in range(len(mapping)):
-        key = lines[i].split(":")[0].lstrip().rstrip()
-        value = float(lines[i].split(":")[2][1:-1])
-        tech_params[mapping[key]] = value  # just know that self.tech_params contains all dat
-        i += 1
-    return tech_params
+        (
+            simulator,
+            self.hw,
+            computation_dfg,
+        ) = architecture_search.setup_arch_search(self.args.benchmark, self.args.architecture_config)
+        self.hw.init_memory(
+            sim_util.find_nearest_power_2(simulator.memory_needed),
+            sim_util.find_nearest_power_2(simulator.nvm_memory_needed),
+        )
+        self.scheduled_dfg = simulator.schedule(computation_dfg, self.hw)
+        hardwareModel.un_allocate_all_in_use_elements(self.hw.netlist)
+        simulator.simulate(self.scheduled_dfg, self.hw)
+        simulator.calculate_edp()
+        logger.warning(f"edp of forward pass: {simulator.edp} E-18 Js")
+        
+        rcs = self.hw.get_optimization_params_from_tech_params()
+        self.initial_tech_params = sim_util.generate_init_params_from_rcs_as_symbols(rcs)
 
-def run_forward_pass():
+    def run_inverse_passes(self, num_iters):
+        symbolic_sim = symbolic_simulate.SymbolicSimulator()
+        symbolic_sim.simulator_prep(self.args.benchmark, self.hw.latency)
+        coefficients.create_and_save_coefficients([self.hw.transistor_size])
+        hardwareModel.un_allocate_all_in_use_elements(self.hw.netlist)
 
-    # initialize hw model, override architecture config to have current tech node
-    logger.info(
-        f"Setting up architecture search; benchmark: {args.benchmark}, config: {args.architecture_config}"
-    )
-    (
-        simulator,
-        hw,
-        computation_dfg,
-    ) = architecture_search.setup_arch_search(args.benchmark, args.architecture_config)
-    hw.init_memory(
-        sim_util.find_nearest_power_2(simulator.memory_needed),
-        sim_util.find_nearest_power_2(simulator.nvm_memory_needed),
-    )
-    scheduled_dfg = simulator.schedule(computation_dfg, hw)
-    hardwareModel.un_allocate_all_in_use_elements(hw.netlist)
-    simulator.simulate(scheduled_dfg, hw)
-    simulator.calculate_edp()
-    logger.warning(f"edp of forward pass: {simulator.edp} E-18 Js")
-    
-    rcs = hw.get_optimization_params_from_tech_params()
-    initial_tech_params = sim_util.generate_init_params_from_rcs_as_symbols(rcs)
+        symbolic_sim.simulate(self.scheduled_dfg, self.hw)
+        symbolic_sim.calculate_edp(self.hw)
 
-def run_inverse_passes(num_iters):
-    symbolic_sim = symbolic_simulate.SymbolicSimulator()
-    symbolic_sim.simulator_prep(args.benchmark, hw.latency)
-    coefficients.create_and_save_coefficients([hw.transistor_size])
-    hardwareModel.un_allocate_all_in_use_elements(hw.netlist)
+        current_tech_params = self.initial_tech_params.copy()
 
-    symbolic_sim.simulate(scheduled_dfg, hw)
-    symbolic_sim.calculate_edp(hw)
-
-    current_tech_params = initial_tech_params.copy()
-
-    for _ in range(num_iters):
-        stdout = sys.stdout
-        with open(f"{args.savedir}/ipopt_out.txt", "w") as sys.stdout:
-            optimize.optimize(current_tech_params, symbolic_sim.edp, "ipopt", regularization=0.1)
-        sys.stdout = stdout
-        f = open(f"{args.savedir}/ipopt_out.txt", "r")
-        current_tech_params = parse_output(f)
+        for _ in range(num_iters):
+            stdout = sys.stdout
+            with open(f"{self.args.savedir}/ipopt_out.txt", "w") as sys.stdout:
+                optimize.optimize(current_tech_params, symbolic_sim.edp, "ipopt", regularization=0.1)
+            sys.stdout = stdout
+            f = open(f"{self.args.savedir}/ipopt_out.txt", "r")
+            current_tech_params = self.parse_output(f)
+            self.record_grads(symbolic_sim.edp, symbolic_sim.cacti_exprs, current_tech_params)
+        
+        self.plot_results()
+        
+        self.plot_3d(symbolic_sim.edp, ["Ceff_Mult", "C_g_ideal"], current_tech_params)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -123,7 +185,7 @@ if __name__ == "__main__":
         help="Path to the save logs",
     )
     parser.add_argument(
-        "N",
+        "-N",
         "--num_iters",
         type=int,
         default="3",
@@ -142,8 +204,11 @@ if __name__ == "__main__":
 
     logging.basicConfig(filename=f"{args.savedir}/log.txt", level=logging.WARNING)
 
+    inv_walkthrough = InverseWalkthrough()
+    inv_walkthrough.args = args
+
     # run forward pass once to setup inverse pass
-    run_forward_pass()
+    inv_walkthrough.run_forward_pass()
 
     # run multiple iterations of inverse pass to demonstrate how gradients of variables change
-    run_inverse_passes(args.num_iters)
+    inv_walkthrough.run_inverse_passes(args.num_iters)
