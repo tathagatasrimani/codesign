62dc1a3980756fae9cb0ab799ebf14d5146fd5b9
Patrick James McEwen <pmcewen@rsgvm14.stanford.edu>
2025-03-02 00:15:26 -0800

    prototype for converting gnt-parsed graph to standard dfg

--- src/parse_schedule.py
+++ src/parse_schedule.py
@@ -4,17 +4,45 @@ import re
 from . import sim_util
 
 class node:
-    def __init__(self, id, name, tp, module):
+    def __init__(self, id, name, tp, module, delay):
         self.id = id
         self.name = name
         self.type = tp
         self.module = module
+        self.delay = delay
 
     def __str__(self):
         return f"========================\nNode id: {self.id}\nName: {self.name}\ntype: {self.type}\nmodule: {self.module}\n========================\n"
 
     def label(self):
         return f"{self.module}-{self.id}-{self.type}"
+    
+def convert_to_standard_dfg(graph: nx.DiGraph, node_objects, module_map):
+    """
+    takes the output of parse_gnt_to_graph and converts
+    it to our standard dfg format for use in the rest of the flow.
+    We discard catapult-specific information and convert each node
+    to one of our standard operators with the correct delay value.
+
+    Arguments:
+    - graph: output of parse_gnt_to_graph
+    - node_objects: mapping of node -> node class object
+    - module_map: mapping from ccore module to standard operator name
+    """
+    modified_graph = nx.DiGraph()
+    for node in graph:
+        if node_objects[node].module in module_map:
+            modified_graph.add_node(
+                node,
+                id=node_objects[node].id,
+                function=module_map[node_objects[node].module],
+                cost=node_objects[node].delay,
+                start_time=0,
+                end_time=0,
+                allocation=""
+            )
+    return modified_graph
+
 
 def parse_gnt_to_graph(file_path):
     """
@@ -37,7 +65,7 @@ def parse_gnt_to_graph(file_path):
             if not line.startswith("set a("):
                 continue
             node_id = line[line.find('(')+1:line.find(')')]
-            tokens = line.split(' ')
+            tokens = line.split()
             node_name = ""
             if (line.find(" NAME ") != -1):
                 for i in range(len(tokens)):
@@ -59,7 +87,14 @@ def parse_gnt_to_graph(file_path):
                         node_module = tokens[i+1]
                         break
                 assert node_module, f"{line}"
-            nodes[node_id] = node(node_id, node_name, node_type, node_module)
+            node_delay = 0
+            if (line.find(" DELAY ") != -1): # todo: investigate if CYCLES need to be added to delay
+                for i in range(len(tokens)):
+                    if (tokens[i] == "DELAY"):
+                        node_delay = float(tokens[i+1][1:-1])
+                #print(node_delay)
+
+            nodes[node_id] = node(node_id, node_name, node_type, node_module, node_delay)
         
 
         for line in lines:
