1e6ff9982e44d17b36687abad85798bd53514251
Patrick James McEwen <pmcewen@rsg-login.stanford.edu>
2025-03-13 19:33:50 -0700

    fix

--- src/benchmarks/matmult/src/matmult.cpp
+++ src/benchmarks/matmult/src/matmult.cpp
@@ -21,17 +21,8 @@ class MatMult {
                 PackedInt2D<PRECISION, 10, 10> a = a_chan.read();
                 PackedInt2D<PRECISION, 10, 10> b = b_chan.read();
                 PackedInt2D<PRECISION, 10, 10> c;
-                PackedInt2D<PRECISION, TILE_SIZE, TILE_SIZE> add_2;
                 PackedInt2D<PRECISION, TILE_SIZE, TILE_SIZE> add_out;
-                PackedInt2D<PRECISION, TILE_SIZE, TILE_SIZE> mul_1;
-                PackedInt2D<PRECISION, TILE_SIZE, TILE_SIZE> mul_2;
                 PackedInt2D<PRECISION, TILE_SIZE, TILE_SIZE> mul_out;
-                for (int i = 0; i < TILE_SIZE; i++) {
-                    for (int j = 0; j < TILE_SIZE; j++) {
-                        mul_inst[i][j].run(mul_1.value[i].value[j], mul_2.value[i].value[j], mul_out.value[i].value[j]);
-                        add_inst[i][j].run(mul_out.value[i].value[j], add_2.value[i].value[j], add_out.value[i].value[j]);
-                    }
-                }
                 //#pragma hls_unroll yes
                 for (int i = 0; i < 10; i++) {
                     //#pragma hls_unroll yes
@@ -58,9 +49,8 @@ class MatMult {
                                 for (int ii = 0; ii < TILE_SIZE; ii++) {
                                     #pragma hls_unroll yes
                                     for (int jj = 0; jj < TILE_SIZE; jj++) {
-                                        mul_1.value[ii].value[jj] = a_tmp.value[ii].value[kk];
-                                        mul_2.value[ii].value[jj] = b_tmp.value[kk].value[jj];
-                                        add_2.value[ii].value[jj] = c_tmp.value[ii].value[jj];
+                                        mul_inst[ii][jj].run(a_tmp.value[ii].value[kk], b_tmp.value[kk].value[jj], mul_out.value[ii].value[jj]);
+                                        add_inst[ii][jj].run(mul_out.value[ii].value[jj], c_tmp.value[ii].value[jj], add_out.value[ii].value[jj]);
                                         c_tmp.value[ii].value[jj] = add_out.value[ii].value[jj];
                                     }
                                 }
