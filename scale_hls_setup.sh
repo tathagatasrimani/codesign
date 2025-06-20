12254f1c0d79bc003229acd1bfbbb9d20b50cc23
Eric Dubberstein <eric@dubberstein.com>
2025-06-19 21:51:41 -0700

    scale_hls_init script done.

--- scale_hls_setup.sh
+++ scale_hls_setup.sh
@@ -3,4 +3,8 @@ git submodule update --init scalehls
 cd scalehls
 sed -i 's|git@github\.com:|https://github.com/|g' .gitmodules
 git submodule update --init polygeist
+cd polygeist
+git submodule update --init llvm-project
+cd ../..
+
 
