39e889f3f732003b6216a3887734c58198646d5b
Eric Dubberstein <eric@dubberstein.com>
2025-07-04 21:42:00 -0700

    deleted some of eric's old test directories

--- src/benchmarks/c_cores_work/c_core.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script converts the output rtl from catapult to the graph netlist format for all C-Cores. 
-
-# $1 is the input verilog file
-# $2 is the output graph netlist file
-
-yosys -p "read_verilog $1; write_json netlist.json"
-
-python3 verilog_parse.py netlist.json $2
