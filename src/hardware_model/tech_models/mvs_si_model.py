39eec9088acfc761d1578c3dc23d1eb43bf13530
patrickmcewen <pmcewen@stanford.edu>
2025-09-24 23:12:38 -0700

    get param db working

--- src/hardware_model/tech_models/mvs_si_model.py
+++ src/hardware_model/tech_models/mvs_si_model.py
@@ -112,7 +112,8 @@ class MVSSiModel(TechModel):
         self.Vdsats = self.vx0 * self.Leff / self.u_n_eff
         self.Vdsat = self.Vdsats * (1 - self.F_f) + self.phi_t * self.F_f
         self.Vdratio = self.V_dsp/self.Vdsat
-        self.Vdbeta = custom_pow(self.Vdratio, self.beta, evaluate=False)
+        #self.Vdbeta = custom_pow(self.Vdratio, self.beta, evaluate=False)
+        self.Vdbeta = self.Vdratio**self.beta
         self.Vdbetabeta = (1 + self.Vdbeta)**(1/self.beta)
         if self.model_cfg["effects"]["F_s"]:
             self.F_s = self.Vdratio / self.Vdbetabeta
@@ -247,23 +248,37 @@ class MVSSiModel(TechModel):
 
     def config_param_db(self):
         super().config_param_db()
-        self.param_db["I_d"] = self.I_d_on
-        self.param_db["C_load"] = self.C_load
-        self.param_db["delay"] = self.delay
-        self.param_db["u_n_eff"] = self.u_n_eff
-        self.param_db["V_th_eff"] = self.V_th_eff
-        self.param_db["delta_vt_dibl"] = (-self.delta * self.V_dsp).xreplace(self.on_state).evalf()
-        self.param_db["R_wire"] = self.R_wire
-        self.param_db["C_wire"] = self.C_wire
+        self.param_db["L"] = self.base_params.L
+        self.param_db["W"] = self.base_params.W
         self.param_db["I_sub"] = self.I_sub
-        self.param_db["I_tunnel"] = self.I_tunnel
-        self.param_db["I_d_on_per_um"] = self.I_d_on_per_um
-        self.param_db["I_sub_per_um"] = self.I_sub_per_um
+        self.param_db["V_th"] = self.base_params.V_th
+        self.param_db["V_th_eff"] = self.V_th_eff.xreplace(self.on_state).evalf()
+        self.param_db["V_dd"] = self.base_params.V_dd
+        self.param_db["wire RC"] = self.m1_Rsq * self.m1_Csq
+        self.param_db["I_on"] = self.I_d_on
+        self.param_db["I_on_per_um"] = self.I_d_on_per_um
+        self.param_db["I_off_per_um"] = self.I_d_off_per_um
         self.param_db["I_tunnel_per_um"] = self.I_tunnel_per_um
-        self.param_db["I_d_off_per_um"] = self.I_d_off_per_um
-        self.param_db["Eox"] = self.E_ox
-        self.param_db["Vox"] = self.V_ox
+        self.param_db["I_sub_per_um"] = self.I_sub_per_um
+        self.param_db["DIBL factor"] = self.delta
+        self.param_db["t_ox"] = self.base_params.tox
+        self.param_db["eot"] = self.eot
         self.param_db["scale_length"] = self.scale_length
+        self.param_db["C_load"] = self.C_load
+        self.param_db["C_wire"] = self.C_wire
+        self.param_db["R_wire"] = self.R_wire
+        self.param_db["R_device"] = self.base_params.V_dd/self.I_d_on
+        self.param_db["SS"] = self.S
+        self.param_db["F_f"] = self.F_f_eval
+        self.param_db["F_s"] = self.F_s_eval
+        self.param_db["vx0"] = self.vx0
+        self.param_db["v"] = self.v.xreplace(self.on_state).evalf()
+        self.param_db["t_1"] = self.t_1
+        self.param_db["f"] = self.base_params.f
+        self.param_db["R_s"] = self.R_s
+        self.param_db["R_d"] = self.R_d
+        self.param_db["parasitic capacitance"] = self.C_diff
+        self.param_db["L_ov"] = self.L_ov
         self.param_db["A_gate"] = self.A_gate
 
     def apply_base_parameter_effects(self):
@@ -275,4 +290,4 @@ class MVSSiModel(TechModel):
     def create_constraints(self, dennard_scaling_type="constant_field"):
         super().create_constraints(dennard_scaling_type)
         self.constraints.append(self.delta <= 0.15)
-        
+    
