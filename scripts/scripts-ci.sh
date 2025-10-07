21158578435b71f0916f590c68ad76e7049db6c0
Sankalp Naik <51851043+Sankalp22863@users.noreply.github.com>
2025-10-07 16:55:58 -0400

    Added the script to run the actual tests.

--- scripts/scripts-ci.sh
+++ scripts/scripts-ci.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-set -euo pipefail
+
+set -eo pipefail
 
 echo "🔎 Linting…"
 # Examples:
@@ -8,9 +9,53 @@ echo "🔎 Linting…"
 # cmake -S . -B build && cmake --build build --target format-check
 
 echo "🧪 Running tests…"
-# Examples:
-# pytest -q --maxfail=1 --disable-warnings --junitxml=reports/test-results.xml
-# npm test -- --ci
-# ctest --test-dir build --output-on-failure
+
+source miniconda3/etc/profile.d/conda.sh
+
+
+# Check if a conda environment is already active
+if [[ -z "${CONDA_DEFAULT_ENV:-}" ]]; then
+  echo "No conda environment active. Activating 'codesign'..."
+  # Adjust 'myenv' to the environment name you want
+  conda activate codesign
+  echo "✅ Conda environment Activated successfully!"
+else
+  echo "✅ Conda environment already active: $CONDA_DEFAULT_ENV"
+fi
+
+
+############### Add useful alisas ###############
+
+shopt -s expand_aliases
+
+alias create_checkpoint="python3 -m test.checkpoint_controller"
+alias run_codesign="python3 -m src.codesign"
+
+echo "Activated the alias succesfully!"
+
+# run_codesign --config vitis_gemm_checkpoint_after_pd
+set +e
+run_codesign --config vitis_gemm_checkpoint_after_pd > run.log 2>&1
+status=$?
+set -e
+
+if grep -q "AssertionError" run.log; then
+  echo "✅ Expected AssertionError found. Treating as success."
+  cat run.log
+  exit 0
+elif [ $status -ne 0 ]; then
+  echo "❌ Unexpected error occurred"
+  cat run.log
+  exit $status
+else
+  echo "✅ Script completed successfully"
+  cat run.log
+  exit 0
+fi
+
+Examples:
+pytest -q --maxfail=1 --disable-warnings --junitxml=reports/test-results.xml
+npm test -- --ci
+ctest --test-dir build --output-on-failure
 
 echo "✅ All checks passed."
