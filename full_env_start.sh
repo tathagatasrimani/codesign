4d06d9f819cc373c33fb817138af548c9b965826
patrickmcewen <pmcewen@stanford.edu>
2025-11-16 18:07:08 -0500

    fix path issue pointing to the wrong python

--- full_env_start.sh
+++ full_env_start.sh
@@ -140,13 +140,13 @@ source "$SETUP_SCRIPTS_FOLDER"/scale_hls_setup.sh $FORCE_FULL # setup scalehls
 ################### SET UP CONDA ENVIRONMENT ##################
 # Check if the directory miniconda3 exists
 if [ -d "miniconda3" ]; then
-    export PATH="$(pwd)/miniconda3/bin:$PATH"
+    export PATH="$(pwd):$PATH"
     source miniconda3/etc/profile.d/conda.sh
 else
     # Install and set up environment
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     bash Miniconda3-latest-Linux-x86_64.sh -b -p "$(pwd)/miniconda3"
-    export PATH="$(pwd)/miniconda3/bin:$PATH"
+    export PATH="$(pwd):$PATH"
     source miniconda3/etc/profile.d/conda.sh
     conda env create -f "$SETUP_SCRIPTS_FOLDER"/environment_simplified.yml
 
