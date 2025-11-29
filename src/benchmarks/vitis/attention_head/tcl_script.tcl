cfdf5d13ba897c629140e30d81a319f7541d37c5
patrickmcewen <pmcewen@stanford.edu>
2025-11-29 04:51:19 -0500

    add new tests

--- src/benchmarks/vitis/attention_head/tcl_script.tcl
+++ src/benchmarks/vitis/attention_head/tcl_script.tcl
@@ -1,6 +1,6 @@
-open_project bitnet
-set_top bitnet
-add_files bitnet.cpp
+open_project attention_head
+set_top attention_head
+add_files attention_head.cpp
 open_solution "solution1" -flow_target vitis
 set_part {xcvu11p-flga2577-1-e}
 create_clock -period 10 -name default
