195c314aaa8034a3c1d71e349853b7d4863d72d4
patrickmcewen <pmcewen@stanford.edu>
2025-12-02 20:51:53 -0500

    add a bunch of new benchmarks

---
+++ src/benchmarks/vitis/aes/tcl_script.tcl
@@ -0,0 +1,9 @@
+open_project aes
+set_top aes
+add_files aes.cpp
+open_solution "solution1" -flow_target vitis
+set_part {xcvu11p-flga2577-1-e}
+create_clock -period 10 -name default
+csynth_design
+#export_design ## we do not need the vivado IP block
+exit
