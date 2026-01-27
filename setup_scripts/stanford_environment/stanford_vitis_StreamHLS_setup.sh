8690be5b5996cc5651cabace4e46c5a2c4537267
Patrick McEwen <pmcewen@stanford.edu>
2026-01-27 15:15:56 -0800

    fix stanford vitis script and update streamhls vitis to 2024 with fix in streamhls submodule

--- setup_scripts/stanford_environment/stanford_vitis_StreamHLS_setup.sh
+++ setup_scripts/stanford_environment/stanford_vitis_StreamHLS_setup.sh
@@ -1,3 +1,3 @@
 ## This script sets up the environment for Vitis HLS to be used with StreamHLS preprocessing tools on Stanford systems.
-
-module load vitis/2022.1
+source /etc/profile.d/modules.sh
+module load vitis/2024.2
