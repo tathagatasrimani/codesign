6f0e50c53aa8924c38cd4d7f9b3e9ab283cbaa54
Daniel Zhao <dzhao2@andrew.cmu.edu>
2026-02-01 21:25:58 -0500

    Initial Version of Tiered Appraoch

--- openroad_interface/tcl/codesign_flow.tcl
+++ openroad_interface/tcl/codesign_flow.tcl
@@ -18,6 +18,11 @@ place_pins -random -hor_layers $io_placer_hor_layer -ver_layers $io_placer_ver_l
 
 ################################################################
 # Macro Placement (using rtl_macro_placer)
+
+# Updated Tiered Placement flow
+
+puts "Step 1: Initial Placement of Macros"
+
 rtl_macro_placer \
     -target_util 0.25 \
     -target_dead_space 0.05 \
@@ -33,7 +38,179 @@ rtl_macro_placer \
     -halo_width 10 \
     -halo_height 10 \
     -report_directory reports \
-    -write_macro_placement macro_place.tcl
+    -write_macro_placement initial_macro_place.tcl
+
+# Configuring for Custom Placement of Muxes
+set mux_prefix "Mux"
+set final_file "macro_place.tcl"
+set halo_x 10
+set halo_y 10
+
+set db [ord::get_db]
+set block [[$db getChip] getBlock]
+set tech [$db getTech]
+set dbu [$tech getDbUnitsPerMicron]
+set core_box [$block getCoreArea]
+
+# Helper for microns/dbu conversion
+proc mic2db {val dbu} { return [expr {round($val * $dbu)}] }
+proc db2mic {val dbu} { return [expr {$val / double($dbu)}] }
+
+# List to track Mux placements to avoid Mux-Mux overlaps
+set placed_mux_bboxes {}
+
+# Helper functions
+
+# Check if a candidate box overlaps with any existing Mux boxes (including Halo)
+proc check_overlap {candidate_box placed_list halo_x_db halo_y_db} {
+    set c_xlo [$candidate_box xMin]
+    set c_ylo [$candidate_box yMin]
+    set c_xhi [$candidate_box xMax]
+    set c_yhi [$candidate_box yMax]
+
+    # Inflate candidate by halo for the check
+    set check_box [odb::Rect \
+        [expr {$c_xlo - $halo_x_db}] \
+        [expr {$c_ylo - $halo_y_db}] \
+        [expr {$c_xhi + $halo_x_db}] \
+        [expr {$c_yhi + $halo_y_db}] \
+    ]
+
+    foreach other_box $placed_list {
+        if { [$check_box intersection $other_box] } {
+            return 1 ;# Overlap detected
+        }
+    }
+    return 0
+}
+
+# Clamp a point (x, y) to be inside a given Box (Region/Die)
+proc clamp_to_box {x y box} {
+    set b_xlo [$box xMin]
+    set b_ylo [$box yMin]
+    set b_xhi [$box xMax]
+    set b_yhi [$box yMax]
+
+    if {$x < $b_xlo} {set x $b_xlo}
+    if {$x > $b_xhi} {set x $b_xhi}
+    if {$y < $b_ylo} {set y $b_ylo}
+    if {$y > $b_yhi} {set y $b_yhi}
+    
+    return [list $x $y]
+}
+
+# Time to start Mux placement
+puts "Step 2: Relocating ${mux_prefix} Macros to allow overlaps with other Macros"
+
+# Open final macro placement file for writing
+set fp [open $final_file "w"]
+
+set insts [$block getInsts]
+
+foreach inst $insts {
+  set inst_name [$inst getName]
+
+  # If instance is a Mux, attempt to place it with overlap allowance
+  if {[string match "${mux_prefix}*" $inst_name]} {
+
+    # Calculate centroid of connectivity
+    set sum_x 0; set sum_y 0; set count 0
+    set iterms [$inst getITerms]
+
+    foreach iterm $iterms {
+      set net [$iterm getNet]
+      if {$net == "NULL"} {continue}
+
+      foreach other_iterm [$net getITerms] {
+        set other_inst [$other_iterm getInst]
+
+        # Don't consider self
+        if {$other_inst == $inst} {continue}
+
+        set loc [$other_inst getLocation]
+        set sum_x [expr {$sum_x + [lindex $loc 0]}]
+        set sum_y [expr {$sum_y + [lindex $loc 1]}]
+        incr count
+      }
+    }
+
+    # If no connections, use original location
+    if {$count == 0} {
+      set current_loc [$inst getLocation]
+      set target_x [lindex $current_loc 0]
+      set target_y [lindex $current_loc 1]
+    } else {
+      set target_x [expr {$sum_x / $count}]
+      set target_y [expr {$sum_y / $count}]
+    }
+
+    # Apply fence & region constraints
+    set region [$inst getRegion]
+    if {$region != "NULL"} {
+      # Get the boundaries
+      set region_box [lindex [$region getBoundaries] 0]
+      set clamped [clamp_to_box $target_x $target_y $region_box]
+      set target_x [lindex $clamped 0]
+      set target_y [lindex $clamped 1]
+    } else {
+      # Clamp to core area
+      set clamped [clamp_to_box $target_x $target_y $core_box]
+      set target_x [lindex $clamped 0]
+      set target_y [lindex $clamped 1]
+    }
+
+    # Resolve Mux-Mux overlaps with Halo consideration
+    set master [$inst getMaster]
+    set w [$master getWidth]
+    set h [$master getHeight]
+    
+    set halo_x_db [mic2db $halo_x $dbu]
+    set halo_y_db [mic2db $halo_y $dbu]
+
+    set attempt 0
+    set max_attempts 10
+    set placed_legal 0
+
+    while {$attempt < $max_attempts} {
+      set x_end [expr {$target_x + $w}]
+      set y_end [expr {$target_y + $h}]
+      set candidate_bbox [odb::Rect $target_x $target_y $x_end $y_end]
+
+      if {![check_overlap $candidate_bbox $placed_mux_bboxes $halo_x_db $halo_y_db]} {
+        set placed_legal 1
+        lappend placed_mux_bboxes $candidate_bbox
+        break
+      }
+
+      # Simple spiral/shift strategy if overlap found
+      set shift_amount [expr {$w + $halo_x_db}]
+      set target_x [expr {$target_x + $shift_amount}]
+      incr attempt
+    }
+    
+    if {!$placed_legal} {
+      puts "Warning: Could not find overlap-free spot for $inst_name. Placing at last attempt."
+    }
+
+    # Time to place the Mux
+    set final_x_db [db2mic $target_x $dbu]
+    set final_y_db [db2mic $target_y $dbu]
+
+    $inst setLocation $target_x $target_y
+    $inst setPlacementStatus "PLACED"
+
+    # Write placement command to final macro placement file
+    puts $fp "place_macro -macro_name $inst_name -location \"$final_mic_x $final_mic_y\" -allow_overlap"
+  } else {
+    # Non-Mux macros retain their original placement
+    set loc [$inst getLocation]
+    set mic_x [db2mic [lindex $loc 0] $dbu]
+    set mic_y [db2mic [lindex $loc 1] $dbu]
+    puts $fp "place_macro -macro_name $inst_name -location \"$mic_x $mic_y\""
+  }
+}
+close $fp
+puts "Macro placement with overlap handling completed. See $final_file for details."
 
 # Lock macro positions by sourcing the generated macro placement file
 #source macro_place.tcl
